bc0f577770cb919c3b6d9327a428899bdce8c233
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-29 12:48:56 +0500

    feat: add api endpoints base

--- backend/HallOfQuestions/HallOfQuestions.Backend/Program.cs
+++ backend/HallOfQuestions/HallOfQuestions.Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -7,5 +9,26 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/api/conferences", async () =>
+{
+
+});
+
+app.MapGet("/api/conferences/{id}/questions", async ([FromRoute] string id) =>
+{
+    
+});
+
+app.MapPost("/api/conferences/{id}/questions", async ([FromRoute] string conferenceId) =>
+{
+
+});
+
+app.MapPost("/api/conferences/{conferenceId}/questions/{questionId}/like",
+    async ([FromRoute] string conferenceId, [FromRoute] string questionId) =>
+    {
+
+    });
 
 app.Run();
