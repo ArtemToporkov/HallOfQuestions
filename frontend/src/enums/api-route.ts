ee705516bb283cdf47abe4cec9203c4bf66a5bfd
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-30 10:36:13 +0500

    feat(front): add routes

---
+++ frontend/src/enums/api-route.ts
@@ -0,0 +1,8 @@
+﻿export enum ApiRoute {
+    Conferences = '/conferences',
+    Questions = '/conferences/:id/questions',
+    StartConference = '/conferences/:id/start',
+    EndConference = '/conferences/:id/end',
+    LikeQuestion = '/conferences/:conferenceId/questions/:questionId/like',
+    UnlikeQuestion = '/conferences/:conferenceId/questions/:questionId/unlike'
+}
---
+++ frontend/src/enums/app-route.ts
@@ -0,0 +1,4 @@
+﻿export enum AppRoute {
+    Conferences = '/conferences',
+    Conference = '/conferences/:id'
+}
